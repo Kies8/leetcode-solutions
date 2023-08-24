70b3f1ce2406f5900456471848165deb53516a67
chiesalorenzo <lorechiesa57@gmail.com>
2023-08-24 14:32:19 +0200

    ⚡️ Optimize solution 169

--- easy/169.majority-element.py
+++ easy/169.majority-element.py
@@ -1,15 +1,33 @@
 from typing import List
 
 
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         obj = {}
+#         for num in nums:
+#             if num in obj:
+#                 obj[num] += 1
+#             else:
+#                 obj[num] = 1
+
+#         for key, value in obj.items():
+#             if value > (len(nums) / 2):
+#                 return key
+
+
+# Optimized Solution Boyer-Moore
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        obj = {}
-        for num in nums:
-            if num in obj:
-                obj[num] += 1
-            else:
-                obj[num] = 1
+        candidate = None
+        count = 0
 
-        for key, value in obj.items():
-            if value > (len(nums) / 2):
-                return key
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
