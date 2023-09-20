18cbde877721b8c8c881c44e6fe2822d93769a80
chiesalorenzo <lorechiesa57@gmail.com>
2023-09-20 23:31:55 +0200

    ✅ solution 1

---
+++ easy/1.two-sums.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = {}
+
+        for i in range(len(nums)):
+            if nums[i] in res:
+                return [res[nums[i]], i]
+
+            res[target - nums[i]] = i
+        
+        return []
